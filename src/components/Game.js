b35f20fc1932d287cb2c19bdd47194010e2b6b66
Michael Rios <michael.rios093@gmail.com>
2021-04-08 13:03:08 -0400

    reload finally works

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,11 +1,10 @@
+
+
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { destroyGame, updateGame } from '../store/';
+import { destroyGame, updateGame, loadHostedGames, loadSingleGame } from '../store/';
 import moment from 'moment';
 import axios from 'axios';
-import CourtMap from './CourtMap'
-
-const COURT_API = process.env.COURT_API
 
 //this will list the games a user is hosting, they will be able to update the time, location, delete the game
 //they will also be able to add a winner and update the score, not sure if these two things should be seperated out 
@@ -16,30 +15,64 @@ export class Game extends Component{
   constructor(props){
     super(props);
     this.state = {
-      location: this.props.game.location ? this.props.game.location : '',
-      dateAndTime: this.props.game.dateAndTime ? this.props.game.dateAndTime : '',
-      time: this.props.game.time ? this.props.game.time : '',
-      finalScore: this.props.game.finalScore ? this.props.game.finalScore : '',
-      winner: this.props.game.winner ? this.props.game.winner : '',
-      done: this.props.game.done ? this.props.game.done : '',
-      error: '',
-      zipcode: '',
-      courts: [],
-      searched: false
+      id: this.props.games.single.id ? this.props.games.single.id : '',
+      location: this.props.games.single.location ? this.props.games.single.location : '',
+      dateAndTime: this.props.games.single.dateAndTime ? this.props.games.single.dateAndTime : '',
+      time: this.props.games.single.time ? this.props.games.single.time : '',
+      finalScore: this.props.games.single.finalScore ? this.props.games.single.finalScore : '',
+      winner: this.props.games.single.winner ? this.props.games.single.winner : '',
+      done: this.props.games.single.done ? this.props.games.single.done : '',
+      maxPlayerCount: this.props.games.single.maxPlayerCount ? this.props.games.single.maxPlayerCount : '',
+      error: ''
   };
 
   this.onChange = this.onChange.bind(this);
   this.onSave = this.onSave.bind(this);
-  this.courtSubmit = this.courtSubmit.bind(this)
-  this.handleInputs = this.handleInputs.bind(this)
 }
-componentDidUpdate(prevProps){
-  //does not mater for the moment as refresh just logs you off
-  if (!prevProps.game.id && this.props.game.id){
-      this.setState({ location: this.props.game.location, dateAndTime: this.props.game.dateAndTime, time: this.props.game.time, finalScore: this.props.game.finalScore, winner: this.props.game.winner, done: this.props.game.done });
-      // console.log(this.props);
-  }
+
+componentDidMount(prevProps){
+
+  const gameId = this.props.location.pathname.slice(7) * 1;
+  
+  console.log(this.props.location.pathname.slice(7));
+  // console.log(prevProps);
+  this.props.bootstrap(gameId);
+  console.log(this.props);
+  
+ 
 }
+
+componentDidUpdate(prevProps, prevState){
+  // console.log(this.props);
+  console.log(prevProps);
+  console.log(this.props);
+
+  console.log(prevState);
+  console.log(this.state.id);
+  console.log(this.props.games.single.id)
+  const gameId = this.props.location.pathname.slice(7) * 1;
+  this.props.bootstrap(gameId)
+  //not catching it when  
+  if (this.props.games.single.id * 1 !== prevState.id * 1 ){
+   
+      console.log(this.props.games.single);
+      this.setState({ id: this.props.games.single.id, 
+        location: this.props.games.single.location, 
+        dateAndTime: this.props.games.single.dateAndTime, 
+        time: this.props.games.single.time, 
+        finalScore: this.props.games.single.finalScore, 
+        winner: this.props.games.single.winner, 
+        done: this.props.games.single.done,
+        maxPlayerCount: this.props.games.single.maxPlayerCount });
+    }
+}
+
+componentWillUnmount(){
+  console.log('uuuuuuuuuuuuuuuuuuuuuuuuuu')
+  // this.setState({ id: ''})
+  // this.state.id = ''
+}
+
 onChange(ev){
   const change = {};
   change[ev.target.name] = ev.target.value;
@@ -47,28 +80,24 @@ onChange(ev){
 }
 async onSave(ev){
   ev.preventDefault();
+  
   try {
-      await this.props.update(this.props.game.id, this.state);
+     
+    await this.props.update(this.props.games.single.id, this.state);
+      
   }
   catch(ex){
       // console.log(ex);
       this.setState({ error: ex.response});
   }
-}
-
-async courtSubmit(ev){
-  ev.preventDefault()
-  const courts =  (await axios.get(`https://data.cityofnewyork.us/resource/9wwi-sb8x.json?$$app_token=${COURT_API}&basketball=Yes&zipcode=${this.state.zipcode}`)).data
-  this.setState({courts: courts, searched: true})
-}
-handleInputs(ev){
-  const {name, value} = ev.target
-  this.setState({[name] : value})
+  this.setState({ id: '' }); 
 }
 
   render(){
-    const { game, destroy } = this.props;
-    const { location, finalScore, error, winner, time } = this.state;
+    const { destroy } = this.props;
+    // console.log(this.props.games.single);
+    const game = this.props.games.single;
+    const { location, finalScore, error, winner, maxPlayerCount } = this.state;
     let { dateAndTime } = this.state;
     const { onChange, onSave} = this;
     // console.log(this.state)
@@ -80,7 +109,7 @@ handleInputs(ev){
     if(Date.now() > game.time * 1){
       willPlay = false;
     } 
-    console.log(this.props)
+        
     return (
         <div>
           <form onSubmit = { onSave }>
@@ -90,105 +119,71 @@ handleInputs(ev){
                 }
             </pre>
             {willPlay ? (
-              <div className='container justify-content-center'>
+              <div className='container'>
                 <h4>This Game will be played on :</h4>
-                {/* ideally this would hring up the map again not a clue how to do that might ask Taylor */}
+                {/* ideally this would bring up the map again not a clue how to do that might ask Taylor */}
                 <p>Location</p>  
-                {!this.state.searched ? (
-                  <div>
-                    <label htmlFor='zipcode'>Zipcode:</label>
-                    <input type="text" id="zipcode" name="zipcode" onChange={this.handleInputs}/>
-                    <button onClick={this.courtSubmit}>Find Courts</button>
-                  </div>
-                ): (
-                  <div className='courtFinder'>
-                    <div className= 'courtForm'>
-                      <label htmlFor='court'>Court:</label>
-                      <select onChange={this.handleInputs} name='location' value={ location }>
-                        <option>Select One</option>
-                        {this.state.courts.map((court, idx)=>{
-                          return(<option key={idx} value={idx} >Court: {court.objectid}</option>)
-                        })}
-                      </select>      
-                    </div>
-                    <div className='courtMap'>
-                      <CourtMap courts={this.state.courts}/>
-                    </div>
-                  </div>
-                )}
-                <label>Current Location:</label>
                 <input name='location' value={ location } onChange = { onChange }/>
                 <br/>
+                <p>Max number of players</p>  
+                <input name='maxPlayerCount' value={ maxPlayerCount } onChange = { onChange }/>
+                <br/>
                 <label htmlFor='date'>Date and Time:</label>
-                <hr/>
+                <br/>
                 {/* Time is a pain, there is a weird extra character on ours */}
-                <input type="dateTime-local" value={ dateAndTime }  className='form-control' name="dateAndTime" onChange={ onChange }/>   
-                <hr></hr>
+                <input type="dateTime-local" value={ dateAndTime } name="dateAndTime" onChange={ onChange }/>   
+                <br/>
                 <h4>Please change the Time or Location</h4>
-                <button className='btn btn-primary' disabled = { !location || !dateAndTime } >SAVE</button>
-                
+                <button disabled = { !location || !dateAndTime || !maxPlayerCount } >SAVE</button>
+                <br/>
                 {/* <button onClick={()=>destroy(game)}>delete this game</button> */}
               </div> 
 
             ) :  (
               
-              <div className='container justify-content-center'>
+              <div className='container'>
                 <h4>This game was played on: </h4> 
-                <hr></hr>
                 <h4>Date: { moment(game.dateAndTime).format('MMM D, YYYY') }</h4>
-                <hr></hr>
                 <h4>Time: { moment(game.dateAndTime).format('h:mm a') }</h4>
-                <hr></hr>
                 <h4>number of players: {game.users ? game.users.length : 0}</h4>               
-                <hr></hr>
                 <h4>Please update the final score and winner: </h4> 
-                <hr></hr>
+                
                 <p>Final Score</p> 
                 {/* would be better if there was error checking */}
-                <input name='finalScore' value={ finalScore } className = 'form-control' onChange = { onChange }/>
-                <hr/>
+                <input name='finalScore' value={ finalScore } onChange = { onChange }/>
+                <br/>
                 {/* will make this a drop down menu of just TEAM A and TEAM B for the moment */}
                 <p>Winner</p> 
                 {/* <input name='winner' value={ winner } onChange = { onChange }/> */}
-                <select name='winner' value={ winner } className = 'form-control' onChange = { onChange }>
+                <select name='winner' value={ winner } onChange = { onChange }>
                     {/* so this need to be linked with the the actual schools and I need to figure 
                     out how to do the update but one step at a time */}
                     <option value = ''>How won??</option>
                     <option value = 'TEAM A'>TEAM A</option>
                     <option value = 'TEAM B'>TEAM B</option>
                 </select>
-                <button className='btn btn-primary' disabled = { !finalScore || !winner }>SAVE</button>
-                <hr/>
+                <button disabled = { !finalScore || !winner }>SAVE</button>
+                <br/>
                 {/* <button onClick={()=>destroy(game)}>delete this game</button> */}
               </div>
-              
-              
-              
             )  }
-            
-
           </form>
-          <div className='container justify-content-center'>
+          <div className='container'>
           <h4>This will permanately delete your game</h4>  
-          <button className='btn btn-danger' onClick={()=>destroy(game)}>delete this game</button>
+          <button onClick={()=>destroy(game)}>delete this game</button>
           </div>         
         </div>            
     );
-  
+ 
   }
 }
 
-const mapStateToProps = (state, otherProps) => {
-  // console.log(otherProps.match);
-  // console.log(state)
-  const game = state.games.hosted.find(game => game.id === otherProps.match.params.id * 1) || {};
-  //console.log(game)
-  return {
-    game, 
-  };
+const mapStateToProps = ( state ) => {
+  return state;
 }
 
-const mapDispatchToProps = (dispatch, { history }) => {
+const mapDispatchToProps = (dispatch, { history, match }) => {
+  // console.log(match.params.id)
   return {
     destroy: (game)=> {
       dispatch(destroyGame(game, history));
@@ -196,11 +191,14 @@ const mapDispatchToProps = (dispatch, { history }) => {
     },
     update: (id, state)=> {
       // console.log('hi');
+     
       dispatch(updateGame(id, state, history));
-    }
+    },
+    bootstrap: (gameId)=> {
+      dispatch(loadSingleGame(gameId));
+    }  
   };
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Game);
-
 
